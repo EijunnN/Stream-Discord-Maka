50da7771d9212ade99cbef56f093eb279497a838
Arturo Meza <arturo97xd@gmail.com>
2024-08-08 19:23:33 -0500

    add channel gabyriba

--- src/index.ts
+++ src/index.ts
@@ -333,6 +333,7 @@ streamer.client.on("messageCreate", async (message) => {
           console.error("Error processing Jkanime URL:", error);
           message.reply(`**An error occurred: ${error.message}**`);
         }
+        break;
       case "ytplay":
         if (streamStatus.joined) {
           message.reply("**Already joined**");
