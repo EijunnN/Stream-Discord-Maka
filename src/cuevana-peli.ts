3af9d2e102fa992027faecb9f76acebbf09ce19e
Arturo Meza <arturo97xd@gmail.com>
2024-08-10 17:39:49 -0500

    fix cuevana peli7

--- src/cuevana-peli.ts
+++ src/cuevana-peli.ts
@@ -327,6 +327,7 @@ async function tryFetchM3U8(lang: string, cyberlocker: string, videoUrl: string)
 
 export const getM3U8FromCuevana2 = async (url: string): Promise<string> => {
   const movie = await fetchDataMovie(url);
+  console.log(movie,"xd")
   if (!movie) {
     throw new Error("No se encontraron datos de video");
   }
